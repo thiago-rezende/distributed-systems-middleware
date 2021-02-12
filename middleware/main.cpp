df31718988c18b1d8ecdb958d6511f6a774969c9
Thiago Rezende <bitrateti@gmail.com>
2021-02-12 02:04:57 -0300

    feat: added cli args parsing with clipp

--- middleware/main.cpp
+++ middleware/main.cpp
@@ -4,22 +4,79 @@
  *
  */
 
+/* Args parser */
+#include <clipp.h>
+
+/* WebSocketpp stuff */
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
 /* Server type shortcut */
 typedef websocketpp::server<websocketpp::config::asio> server_t;
 
-/* Message Handler */
-void on_message(websocketpp::connection_hdl handle, server_t::message_ptr message)
+/* Handlers */
+void on_open(std::shared_ptr<server_t> server, websocketpp::connection_hdl handle)
 {
     H_PROFILE_FUNCTION();
-    std::cout << message->get_payload() << std::endl;
+
+    server_t::connection_ptr con = server->get_con_from_hdl(handle);
+    std::string res = con->get_resource();
+
+    std::regex channel_regex("(/\\w+)", std::regex_constants::ECMAScript);
+
+    if (!std::regex_search(res, channel_regex))
+    {
+        con->close(websocketpp::close::status::internal_endpoint_error, "invalid channel string");
+        return;
+    }
+
+    H_DEBUG("[CONNECTION] [OPEN] host => [{}] channel => [{}]", con->get_host(), res.substr(1));
+}
+
+void on_close(std::shared_ptr<server_t> server, websocketpp::connection_hdl handle)
+{
+    H_PROFILE_FUNCTION();
+
+    server_t::connection_ptr con = server->get_con_from_hdl(handle);
+    std::string res = con->get_resource();
+
+    std::regex channel_regex("(/\\w+)", std::regex_constants::ECMAScript);
+
+    H_DEBUG("[CONNECTION] [CLOSE] host => [{}] channel => [{}]", con->get_host(), res.substr(1));
+}
+
+void on_message(std::shared_ptr<server_t> server, websocketpp::connection_hdl handle, server_t::message_ptr message)
+{
+    H_PROFILE_FUNCTION();
+
+    server_t::connection_ptr con = server->get_con_from_hdl(handle);
+    std::string res = con->get_resource();
+
+    std::regex channel_regex("(/\\w+)", std::regex_constants::ECMAScript);
+
+    H_DEBUG("[MESSAGE] host => [{}] channel => [{}] message => [{}]", con->get_host(), res.substr(1), message->get_payload());
 }
 
 /* Application Entry Point */
-int main()
+int main(int argc, char **argv)
 {
+    /* Args variables */
+    uint16_t port = 9002;
+
+    /* Set cli options */
+    clipp::group cli(clipp::required("-p", "--port").doc("port to listen on") & clipp::value("port", port));
+
+    /* Parse the args */
+    if (!clipp::parse(argc, argv, cli))
+    {
+        /* Show help */
+        std::cout << clipp::make_man_page(cli, "middleware");
+
+        /* End the profile session */
+        H_PROFILE_END_SESSION();
+        return 0;
+    }
+
     /* Starts Profile Session */
     H_PROFILE_BEGIN_SESSION("Application Profile", "profile_results.json");
 
@@ -33,20 +90,29 @@ int main()
         /* Sync */
         std::atomic<bool> runing = true;
 
-        server_t server;
-        server.set_message_handler(&on_message);
+        std::shared_ptr<server_t> server = std::make_shared<server_t>();
+        server->clear_access_channels(websocketpp::log::alevel::all);
+        server->set_open_handler(std::bind(&on_open, server, std::placeholders::_1));
+        server->set_close_handler(std::bind(&on_close, server, std::placeholders::_1));
+        server->set_message_handler(std::bind(&on_message, server, std::placeholders::_1, std::placeholders::_2));
 
-        server.init_asio();
-        server.listen(9002);
-        server.start_accept();
+        H_DEBUG("[SERVER] Initializing");
+        server->init_asio();
+        server->listen(port);
+        H_DEBUG("[SERVER] Listening on port {}", port);
+        server->start_accept();
+        H_DEBUG("[SERVER] Ready to accept connections");
 
-        std::thread server_thread([&]() { server.run(); });
+        std::thread server_thread([&]() { server->run(); });
+        H_DEBUG("[SERVER] Running");
 
         std::cin.get();
 
-        server.stop();
+        server->stop();
+        H_DEBUG("[SERVER] Terminating");
 
         server_thread.join();
+        H_DEBUG("[SERVER] Stopped");
     }
 
     /* Ends Profile Session */
--- middleware/pch.hpp
+++ middleware/pch.hpp
@@ -6,25 +6,9 @@
 
 #pragma once
 
-/* Windows Stuff */
-#ifdef WIN32
-
-/* Exclude rarely-used services from Windows headers */
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN
-#endif // VC_EXTRALEAN
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif // WIN32_LEAN_AND_MEAN
-
-/* Windows Header */
-#include <Windows.h>
-
-#endif // WIN32
-
 /* StdLib Stuff */
 #include <set>
+#include <regex>
 #include <mutex>
 #include <atomic>
 #include <string>
